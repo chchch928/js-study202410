d8f6bd84e9cbd2736b10e467a5db90d913a808d5
chchch928 <kbanda@naver.com>
2024-10-28 11:24:38 +0900

    js-1-3-데이터타입 학습

---
+++ 1-3데이터타입/1-basic-type.js
@@ -0,0 +1,10 @@
+
+let age = 20;
+console.log(typeof age);  //age의 데이터타입이 뭐야?
+
+// 자바스크립트는 숫자를 전부 실수로 판단
+let taxRate = 0.15;
+console.log(typeof taxRate);  
+
+console.log(age/0); //Infinity
+console.log(age*'메롱'); //NaN : Not a Number
