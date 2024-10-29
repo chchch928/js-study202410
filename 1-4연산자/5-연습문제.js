bcb34121aedfda9656541929e2a229b548d2b3f4
chchch928 <kbanda@naver.com>
2024-10-29 11:30:50 +0900

    1-4연산자완료

---
+++ 1-4연산자/5-연습문제.js
@@ -0,0 +1,21 @@
+let num1 = '100';
+let num2 = 100;
+
+console.log(num1 == num2);
+console.log(num1 === num2);
+console.log(num1 != num2);
+console.log(num1 !== num2);
+
+
+console.log('==============');
+let a = 5;
+let b = 10;
+
+let result1 = (a > 3) && (b < 15);
+let result2 = (a === 5) || (b === 20);
+let result3 = !(a < b);
+
+console.log(result1);
+console.log(result2);
+console.log(result3);
+
---
+++ 1-4연산자/7-practice.js
@@ -0,0 +1,14 @@
+let yourname = prompt('당신의 이름을 알려주세요')
+let age = prompt('당신의 나이를 알려주세요')
+
+alert(`${yourname}씨는 ${Number(2024-Number(age)+1)}년생이시군요`)
+
+//출생년도 구하기 (한국나이)
+//let birthYear = 2024-age+1 로 새 변수 만들기
+//지금 연도는 currentyear
+
+
+
+//1단계로 prompt와 alert와 글씨만 작성한 후 확인
+//2단계로 이름의 변수를 만들고 이름 출력부분에 변수이름 입력
+//3단계로 나이의 변수를 만들고 나이 출력부분에 변수이름 입력
