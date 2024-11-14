c672d8a282f2251d294bd897487a7948841144bf
chchch928 <kbanda@naver.com>
2024-11-14 17:58:58 +0900

    11-14 수정

--- 2-4function-advanced/5-callback-example.js
+++ 2-4function-advanced/5-callback-example.js
@@ -1,50 +1,49 @@
-
 const userList = [
-    {
-      account: "abc1234",
-      userName: "대길이",
-      job: "추노",
-      address: "서울",
-      hobbys: ["수영", "축구", "테니스"],
-    },
-    {
-      account: "banana",
-      userName: "빠나나",
-      job: "과일",
-      address: "서울",
-      hobbys: ["푸드파이팅", "테니스"],
-    },
-    {
-      account: "park1234",
-      userName: "주차왕",
-      job: "발렛파킹",
-      address: "경기",
-      hobbys: ["족구", "축구", "테니스", "영화감상"],
-    },
-    {
-      account: "fire",
-      userName: "불꽃남자카리스마",
-      job: "게이머",
-      address: "서울",
-      hobbys: ["독서", "테니스"],
-    },
-  ];
+  {
+    account: "abc1234",
+    userName: "대길이",
+    job: "추노",
+    address: "서울",
+    hobbys: ["수영", "축구", "테니스"],
+  },
+  {
+    account: "banana",
+    userName: "빠나나",
+    job: "과일",
+    address: "서울",
+    hobbys: ["푸드파이팅", "테니스"],
+  },
+  {
+    account: "park1234",
+    userName: "주차왕",
+    job: "발렛파킹",
+    address: "경기",
+    hobbys: ["족구", "축구", "테니스", "영화감상"],
+  },
+  {
+    account: "fire",
+    userName: "불꽃남자카리스마",
+    job: "게이머",
+    address: "서울",
+    hobbys: ["독서", "테니스"],
+  },
+];
 
-  // 회원목록에서 취미가 딱 2개인 사람들만 추출해서
-  // 새 배열에 담아서 반환해주는 함수를 만드세요
+// 회원목록에서 취미가 딱 2개인 사람들만 추출해서
+// 새 배열에 담아서 반환해주는 함수를 만드세요
 
 //   function filterByHas2hobbies(){
-//     // 새 배열 생성 
+//     // 새 배열 생성
 //     const filteredArray = [];
 //     for( const user of userList){
 //         if (user.hobbys.length === 2){
 //             filteredArray.push(user)
-                
+
 //             }
 //         }
 //         return filteredArray
-//     } 
-  
+//     }
+
 // 회원목록에서 서울사는 사람들만 새 배열에 담아 리턴해줘
 
 // function filterByUserLiveInSeoul(){
@@ -52,16 +51,14 @@ const userList = [
 //     for( const user of userList){
 //         if (user.address === '서울'){
 //             filteredArray.push(user)
-                
+
 //             }
 //         }
 //         return filteredArray
-//     } 
-
+//     }
 
 // const newArray2 = filterByUserLiveInSeoul()
 // console.log(newArray2);
-
 
 // 회원목록에서 경기사는 사람들만 새배열에 담아 리턴해줘
 
@@ -70,44 +67,42 @@ const userList = [
 //     for( const user of userList){
 //         if (user.address === region){
 //             filteredArray.push(user)
-                
+
 //             }
 //         }
 //         return filteredArray
-//     } 
+//     }
 
 // const newArray3 = filterByUserLiveInRegion('경기')
 // console.log(newArray3);
 
-console.log('=================================================');
+console.log("=================================================");
 
-// 회원목록에서 특정조건을 만족하는 회원들을 필터링 
-function filter(condition){
-    const filteredArray = [];
-    for( const user of userList){
-         if (condition(user)){
-             filteredArray.push(user)
-                
-             }
-         }
-         return filteredArray
-
+// 회원목록에서 특정조건을 만족하는 회원들을 필터링
+function filter(condition) {
+  const filteredArray = [];
+  for (const user of userList) {
+    if (condition(user)) {
+      filteredArray.push(user);
+    }
+  }
+  return filteredArray;
 }
 
 // 취미가 4개인 사람을 필터링
 
-const filter1 = filter(function(user){
-    return user.hobbys.length === 4;
-})
+const filter1 = filter(function (user) {
+  return user.hobbys.length === 4;
+});
 console.log(filter1);
 
 const filter2 = filter(function (user) {
-    return user.userName.includes('왕') && user.hobbys.length === 2;
-  });
- console.log(filter2);
-  
-  // 서울 살면서 이름이 8글자인 사람
-  const filter3 = filter(user => user.address === '서울' && user.userName.length === 8);
-  console.log(filter3);
-  
-  
+  return user.userName.includes("왕") && user.hobbys.length === 2;
+});
+console.log(filter2);
+
+// 서울 살면서 이름이 8글자인 사람
+const filter3 = filter(
+  (user) => user.address === "서울" && user.userName.length === 8
+);
+console.log(filter3);
--- 2-5ES6-additional-function/1-destructing.js
+++ 2-5ES6-additional-function/1-destructing.js
@@ -1,5 +1,4 @@
-
-const userNames = ['김철수','홍길동','고구마']
+const userNames = ["김철수", "홍길동", "고구마"];
 
 // 배열에 들어있는 데이터를 다시 각각의 변수에 저장하고 싶음
 
@@ -11,55 +10,53 @@ const [kim, hong, go] = userNames;
 
 console.log(`kim:${kim},hong: ${hong},go:${go}`);
 
-
-// 나는 1번인덱스 말고 0번인덱스와 2번만 추출하고 싶어 
-const[kk,gg] = userNames
+// 나는 1번인덱스 말고 0번인덱스와 2번만 추출하고 싶어
+const [kk, ,gg] = userNames;
 console.log(`kk:${kk},gg:${gg}`);
 
-const exArr = [[],( )=> {}]
+const exArr = [[10,20], () => {}];
 
-const [numbers,foo] = exArr;
-numbers[1];
-foo();
+const [numbers, foo] = exArr;
+numbers[1]; // 20 
+foo(); 
 
-let first =10 , second =20;
+
+let first = 10,
+  second = 20;
 
 // first를 second의 값으로 second를 first의 값으로 변경하고 싶다면
-[first,second]=[second,first]
+[first, second] = [second, first];
 console.log(`first: ${first},second: ${second}`);
-
 
 // 맨 앞 2개의 숫자만 각각의 변수에 저장하고
 // 나머지는 다시한번 배열로 묶어서 보관하고 싶다.
 
-const nums = [1, 3, 5, 7, 9, 11]
+const nums = [1, 3, 5, 7, 9, 11];
 
 // const numsCopy = nums.slice()
 // const one = numsCopy.shift();
 // const three = numsCopy.shift()
 
-const [one , three, ...numsCopy] = nums
+const [one, three, ...numsCopy] = nums;
 
 console.log(`one:${one}`);
 console.log(`three:${three}`);
 console.log(`others:`, numsCopy);
 
 // 스프레드를 통한 배열 합치기
-const foods = ['햄버거','콜라','감자튀김']
-const fruits =['오렌지','자몽','레몬']
+const foods = ["햄버거", "콜라", "감자튀김"];
+const fruits = ["오렌지", "자몽", "레몬"];
 
-const newFoods = [...foods,...fruits]
+const newFoods = [...foods, ...fruits];
 // ...을 넣지 않을 경우 따로 따로 출력됨
 console.log(newFoods);
 console.log(newFoods.length);
 
 console.log(foods);
 const newFruits = [...fruits];
-newFruits[1] = '포도';
+newFruits[1] = "포도";
 console.log(newFruits);
 console.log(fruits);
 
-const myFavoriteFoods = ['족발',...foods, '닭강정','피자']
+const myFavoriteFoods = ["족발", ...foods, "닭강정", "피자"];
 console.log(myFavoriteFoods);
-
-
